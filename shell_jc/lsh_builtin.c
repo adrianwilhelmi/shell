9672a96d34fd718c8de9c62714e61177fa6356ea
adrianwilhelmipwr <268479@student.pwr.edu.pl>
2023-12-20 16:31:48 +0100

    upgraded fg and bg

--- shell_jc/lsh_builtin.c
+++ shell_jc/lsh_builtin.c
@@ -37,6 +37,35 @@ int handle_fg(Command*command){
 		printf("no jobs avail\n");
 		return 1;
 	}
+	
+	if(command->command[1] != NULL){
+		long job_number;
+		char*message;
+		job_number = strtol(command->command[1], &message, 10);
+		if(message == command->command[1]){
+			printf("no such job\n");
+			return 1;
+		}
+		else{
+			if(job_number < 1){
+				printf("no such job\n");
+				return 1;
+			}
+			
+			Job*job_to_continue = first_job;
+			while(job_number != 1 && job_to_continue != NULL){
+				job_to_continue = job_to_continue->next;
+				--job_number;
+			}
+			if(job_to_continue == NULL){
+				printf("no such job\n");
+				return 1;
+			}
+			continue_job(job_to_continue, 0);
+			return 1;
+		}
+	}
+	
 	continue_job(first_job, 0);
 	return 1;
 }
@@ -46,6 +75,35 @@ int handle_bg(Command*command){
 		printf("no jobs avail\n");
 		return 1;
 	}
+	
+	if(command->command[1] != NULL){
+		long job_number;
+		char*message;
+		job_number = strtol(command->command[1], &message, 10);
+		if(message == command->command[1]){
+			printf("no such job\n");
+			return 1;
+		}
+		else{
+			if(job_number < 1){
+				printf("no such job\n");
+				return 1;
+			}
+			
+			Job*job_to_continue = first_job;
+			while(job_number != 1 && job_to_continue != NULL){
+				job_to_continue = job_to_continue->next;
+				--job_number;
+			}
+			if(job_to_continue == NULL){
+				printf("no such job\n");
+				return 1;
+			}
+			continue_job(job_to_continue, 1);
+			return 1;
+		}
+	}
+	
 	continue_job(first_job, 1);
 	return 1;
 }
